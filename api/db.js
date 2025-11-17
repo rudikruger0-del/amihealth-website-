e4a07c1554e180804cbbbfbccd7e6aa852efb9fa
rudikruger0-del <rudikruger0@gmail.com>
2025-11-17 20:26:50 +0200

    Delete api directory

--- api/db.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-});
-
-export default sequelize;
