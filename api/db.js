3ca3c552e841ac2bcac640832d820b9241e063bd
rudikruger0-del <rudikruger0@gmail.com>
2025-11-18 18:52:20 +0200

    Delete api/db.js

--- api/db.js
+++
@@ -1,9 +0,0 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  connectionString: "postgresql://postgres:cPtSxtwKbNMoRoQDXZMcsXCyxQQiiACy@switchback.proxy.rlwy.net:18486/railway",
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
