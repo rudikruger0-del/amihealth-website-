a9c9ce3c4798f202096a1399d2c2a4f4de6fee97
rudikruger0-del <rudikruger0@gmail.com>
2025-11-18 12:07:24 +0200

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,11 +1,3 @@
-{
-  "type": "module",
-  "dependencies": {
-    "pg": "^8.11.1",
-    "bcryptjs": "^2.4.3",
-    "jsonwebtoken": "^9.0.2"
-  }
-}
 export default function handler(req, res) {
   return res.status(200).json({
     status: "AMI backend online",
