170c7dca8ab740705031dfaa16673f490f0f8b13
rudikruger0-del <rudikruger0@gmail.com>
2025-11-19 11:40:27 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../lib/supabaseClient.js";
+import { supabase } from "../lib/supabaseClient.js";
 import bcrypt from "bcryptjs";
 
 export default async function handler(req, res) {
